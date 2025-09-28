a719c444efe86ec3f66dc12dd24e46cf0f8a7bb1
Ankit <ankitmittal0814@gmail.com>
2025-09-28 16:38:31 +0530

    Update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,6 +17,22 @@ export const projects = [
     ],
   },
   {
+    title: "Clean Neighbourhood",
+    // link: "https://oshoppe-ecom.onrender.com/",
+    description:
+      `A community-focused app that enables students and residents to log litter cleanup activities, upload proof, and track service hours, with an admin panel for verifying submissions and managing users.\nIntegrated Firebase notifications for real-time updates and Twilio OTP for secure sign-in.\nDeveloped admin panel APIs for reviewing submissions, verifying service hours, and managing user data.`,
+    stack: [
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Postman",
+      "AWS",
+      "Firebase",
+      "Nodemailer",
+      "Twilio",
+    ],
+  },
+  {
     title: "Oshoppe E-Commerce Platform",
     link: "https://oshoppe-ecom.onrender.com/",
     description:
@@ -31,18 +47,18 @@ export const projects = [
       "AWS S3"
     ],
   },
-  // {
-  //   title: "Chat with Docs ",
-  //   link: "https://chatwithdocs-xflo.onrender.com/",
-  //   description:
-  //     `Developed a full-stack AI-powered web application that enables users to upload PDF or text files and ask questions based on their content.\nImplemented document parsing and semantic search using Node.js, Express, FAISS and transformer-based models to generate embeddings and retrieve contextually relevant answers.`,
-  //   stack: [
-  //     "React",
-  //     "Tailwind CSS",
-  //     "Node.js",
-  //     "Express.js",
-  //     "FAISS",
-  //     "transformer-based models"
-  //   ],
-  // },
+  {
+    title: "Chat with Docs ",
+    link: "https://chatwithdocs-xflo.onrender.com/",
+    description:
+      `Developed a full-stack AI-powered web application that enables users to upload PDF or text files and ask questions based on their content.\nImplemented document parsing and semantic search using Node.js, Express, FAISS and transformer-based models to generate embeddings and retrieve contextually relevant answers.`,
+    stack: [
+      "React",
+      "Tailwind CSS",
+      "Node.js",
+      "Express.js",
+      "FAISS",
+      "transformer-based models"
+    ],
+  },
 ];
